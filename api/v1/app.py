64737b420c43eb6dad0bac6042a5ba9d3dc8a691
bekatadesse <bekatadesse5@gmail.com>
2022-12-29 12:04:11 +0300

    update on api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""app.py to connect to API"""
+"""
+app.py to connect to API
+"""
+
 import os
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(code):
+def teardown_appcontext(self):
     """teardown_appcontext"""
     storage.close()
 
@@ -22,6 +25,8 @@ def teardown_appcontext(code):
 def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    web_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    web_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=web_host, port=web_port, threaded=True)
